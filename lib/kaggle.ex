f92289cb402766249c42aa800c33b838eecd0729
Matt Silbernagel <silbermm@gmail.com>
2017-01-10 20:35:24 -0500

    More rework

--- lib/kaggle.ex
+++ lib/kaggle.ex
@@ -6,37 +6,18 @@ defmodule Kaggle do
   end
 
   def open_file do 
-    File.stream!("./winequality-data.csv") |> CSV.decode(headers: true) |> Enum.to_list
+    File.stream!("./winequality-data.csv") |> CSV.decode |> Enum.to_list
   end
 
   def train_data(data) do
     bayes = SimpleBayes.init()
     IO.puts("starting loop")
     Enum.each(data, fn(d) -> 
-      id = Map.get(d, "id")
-      IO.puts("row for id #{id}")
-      keys = Map.keys(d)
-      keys = Enum.filter(keys, fn(x) -> x != "id" end)
-      train(bayes, id, keys, d)
+      quality = Enum.take(d, 11)
+      d = Enum.drop(d, -2)
+      SimpleBayes.train(bayes, quality, Enum.join(d, " "))
     end)
     IO.puts("end loop")
     bayes
-  end
-
-  def train(bayes_pid, id, [h | []], map) do
-    IO.puts("last column for this row")
-    SimpleBayes.train(
-      bayes_pid,
-      id,
-      "#{h} #{Map.get(map, h)}")
-  end
-
-  def train(bayes_pid, id, [h | t] = keys, map) do
-    IO.puts("#{Enum.count(keys)} columns left")
-    SimpleBayes.train(
-      bayes_pid,
-      id,
-      "#{h} #{Map.get(map, h)}")
-    train(bayes_pid, id, t, map)
   end
 end
