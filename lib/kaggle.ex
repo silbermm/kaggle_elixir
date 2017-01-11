d135f040797be80752b32cd6f8fb9c72ff7ef3de
Matt Silbernagel <silbermm@gmail.com>
2017-01-11 09:09:46 -0500

    got it working

--- lib/kaggle.ex
+++ lib/kaggle.ex
@@ -6,18 +6,19 @@ defmodule Kaggle do
   end
 
   def open_file do 
-    File.stream!("./winequality-data.csv") |> CSV.decode |> Enum.to_list
+    File.stream!("./winequality-data.csv") 
+    |> CSV.decode 
+    |> Enum.to_list
+    |> Enum.drop(1)
   end
 
   def train_data(data) do
     bayes = SimpleBayes.init()
-    IO.puts("starting loop")
     Enum.each(data, fn(d) -> 
-      quality = Enum.take(d, 11)
+      quality = Enum.at(d, 11)
       d = Enum.drop(d, -2)
       SimpleBayes.train(bayes, quality, Enum.join(d, " "))
     end)
-    IO.puts("end loop")
     bayes
   end
 end
